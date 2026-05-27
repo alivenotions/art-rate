063ca467283306e8249f0444f6830f3a7023ebc4
Bhavdeep Dhanjal <bhavdeep@hey.com>
2026-05-27 17:58:00 +0200

    things

--- api/kv/[key].js
+++ api/kv/[key].js
@@ -2,10 +2,7 @@ const { Redis } = require('@upstash/redis');
 
 const ALLOWED_KEYS = ['ratings', 'tweaks'];
 
-const redis = new Redis({
-  url: process.env.KV_REST_API_URL,
-  token: process.env.KV_REST_API_TOKEN,
-});
+const redis = Redis.fromEnv();
 
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
